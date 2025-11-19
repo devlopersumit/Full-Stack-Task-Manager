145cb1c217aed2cb533d13bdc29f1458ad26a543
devlopersumit <sumitsj2002@gmail.com>
2025-11-19 20:45:03 +0530

    created static user authentication

---
+++ server/middlewares/auth.js
@@ -0,0 +1,27 @@
+const userAuth = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader || !authHeader.startsWith('Basic')) {
+        return res.status(401).json({
+            message:'Unauthorized access. Please provide valid credentials.'
+        });
+    }
+
+    try{
+        const base64Credentials = authHeader.split(' ')[1];
+        const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
+        const [username, password] = credentials.split(':');
+
+        if(username === process.env.AUTH_USER && password === AUTH_PASS) {
+            next();
+        } else {
+            return res.status(401).json({
+                message:'Unauthorized access. Please provide valid credentials.'
+            });
+        }
+    }catch(err) {
+        res.status(500).json('Something went wrong' || err.message)
+    }
+};
+
+module.exports = userAuth;
