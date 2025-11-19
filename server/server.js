5ea18a53fe63ceed3742c0bc83d8b2b746f92ed4
devlopersumit <sumitsj2002@gmail.com>
2025-11-19 19:53:39 +0530

    setup DB connection

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server is Running Succesfully at Port ${PORT}`)
+});
