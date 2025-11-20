4ad7c5bc2bbf67adca46be748d037c9c7b247995
devlopersumit <sumitsj2002@gmail.com>
2025-11-20 17:14:21 +0530

    Improve layout spacing and padding

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -5,12 +5,12 @@ import Header from './Header';
 
 const Layout = () => {
   return (
-    <div className="flex h-screen overflow-hidden bg-gray-50">
+    <div className="flex h-screen overflow-hidden bg-[#1a1f2e]">
       <Sidebar />
-      <div className="flex-1 flex flex-col overflow-hidden">
-        <Header />
+      <div className="flex-1 flex flex-col overflow-hidden bg-[#1a1f2e]">
         <main className="flex-1 overflow-y-auto">
-          <div className="container mx-auto px-6 py-8">
+          <div className="px-6 sm:px-8 lg:px-12 py-6 sm:py-8 max-w-7xl mx-auto w-full">
+            <Header />
             <Outlet />
           </div>
         </main>
